dbd0f0fbfe532e5a976d93a2d3e79dda0639f975
UtopiOne <arseniyver@gmail.com>
2023-04-02 12:09:28 +0300

    Initital

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import '../app.postcss';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl">RealTimeX</strong>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '@skeletonlabs/skeleton/styles/all.css';
+</script>
+
+<div class="card p-4">Basic</div>
